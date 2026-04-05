550ef44f6461bc16504200cfec5a6b44ccfa99eb
Marian-Hristov <marianhr2014@gmail.com>
2026-04-04 21:15:23 -0400

    Added mock lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
